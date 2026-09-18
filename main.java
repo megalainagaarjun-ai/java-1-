5471b22445a4507059276b0d80cd3e282a9e25f4
megalainagaarjun-ai <megalainagaarjun@gmail.com>
2026-09-18 15:48:00 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Manimeghalai";
+        int age = 20;
+        String course = "B.Sc Computer Science";
+
+        System.out.println("Student Management System");
+        System.out.println("-------------------------");
+        System.out.println("Name   : " + name);
+        System.out.println("Age    : " + age);
+        System.out.println("Course : " + course);
+    }
+}
